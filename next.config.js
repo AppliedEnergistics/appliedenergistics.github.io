828f8463aa35da564d220384232af9bde9e69f12
Sebastian Hartte <shartte@users.noreply.github.com>
2023-08-13 00:19:34 +0200

    Update workflow

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ module.exports = {
     loader: "custom",
   },
   webpack: (config) => {
-
-    config.optimization.minimize = false;
-
     config.module.rules = [
       ...config.module.rules,
       {
